c071ccbd86408caa25ca5abf017b591b534f4d49
Mariia Penkivska <mariapenkivska13@gmail.com>
2023-05-15 23:49:28 +0300

    Implemented the function which send message to skype user or group

---
+++ sk.py
@@ -0,0 +1,13 @@
+import time
+from skpy import Skype
+from decouple import config
+from datetime import datetime, date
+from bdateutil import isbday
+
+
+while True:
+    now = datetime.now()
+    if now.strftime("%H:%M") == "10:00" and isbday(date(now.year, now.month, now.day)):
+        user = Skype(config('SK_LOGIN'), config('SK_PASS'))
+        user.chats['SK_CHAT_ID'].sendMsg("Hello guys")
+    time.sleep(60)
